4efbbc81a98b874e7ea2f1873b9cc4a764e686fa
Vinicius Pires | UTFPR <viniciuspires@alunos.utfpr.edu.br>
2024-10-10 15:32:03 -0300

    Ta redondo

--- script.sh
+++ script.sh
@@ -1,43 +1,69 @@
 helpFunction() {
-  echo "Usos: $0 -R RegistroAluno -k NomeChave"
-  echo -e "\t-R insira o seu RA"
-  echo -e "\t-k insira o nome da chave que deseja manipular esteja certo sobre seu nome"
+  echo "*****************************************************************************************************************"
+  echo "Usos: $0 -r RegistroAluno"
+  echo -e "\t-r insira o seu RA"
+  echo -e "\t-k nome da chave -> OPCIONAL"
+  echo "*****************************************************************************************************************"
 }
 
-while getopts "R:k:" opt
+while getopts "r:k:" opt;
   do
-    case "$opt" in
-      R ) RA = "$OPTARG" ;;
-      K ) keyname = "$OPTARG" ;;
-      ? ) helpFunction ;; # Imprima helpFunction no caso de algum argumento invalido
+    case $opt in
+      r ) RA=$OPTARG ;;
+      k ) keyname=$OPTARG ;;
+      \? ) 
+        helpFunction 
+        exit 1  ;; # Imprima helpFunction no caso de algum argumento invalido
     esac
   done
 
-if [ -z "$RA" ] || [ -z "$keyname" ];
+if [ -z "$RA" ]
   then
-    echo "Todos ou um dos argumentos estão vazios";
+    echo "#####################################################"
+    echo "Informe seu RA na execução do script com a flag -r";
+    echo "#####################################################"
+    echo "Exemplo: ./script.sh -r 111111"
     helpFunction
+    exit 1
 fi
 
-homePersonalSSH = /media/a$RA/home/.ssh
+if [ -z "$keyname" ]
+  then
+    keyname=id_ed25519
+fi
+
+homePersonalSSH=/media/a$RA/home/.ssh
+
+if [ ! -d $homePersonalSSH ]
+  then
+    echo ""
+    echo "R A incorreto, verifique seu RA e tente novamente"
+    echo ""
+fi
 
 # verifica se ha uma pasta para armazenar as chaves
-if [ !$homePersonalSSH/ ];
-  # caso não exista ira criar uma pasta
+if [ ! -d $homePersonalSSH ];
+  # caso não exista ira criar uma pasta e uma chave
   then 
-    mkdir $homePersonalSSH/
+    mkdir $homePersonalSSH
+    ssh-keygen -t ed25519 -f $homePersonalSSH/$keyname -C $RA
+    echo "Chave $keyname criada com sucesso"
     echo "Criado um diretorio .ssh no $homePersonalSSH"
 fi
 
-# Verifica se ha chaves sendo armazenadas, caso não haja ira criar uma
-if [ -z "(ls -A $homePersonalSSH)" ]
+if [ ! -d ~/.ssh ]
   then
-    ssh-keygen -t ed25519 -f $homePersonalSSH/$keyname -C $RA
-    echo "Chave $keyname criada com sucesso"
-    
+    mkdir ~/.ssh
+    echo "Criado pasta ssh na home do linux "
+fi
 
 # Se a chave ja existe no diretorio .ssh pessoal copie ela pro diretorio do linux
-if [ $homePersonalSSH/$keyname ]
+if [ $homePersonalSSH ]
   then
-    cp -r $homePersonalSSH/ -t ~/.ssh/
-    chmod 400 ~/.ssh/$keyname
+    if [ ! -z "$(ls -A ~/.ssh)" ] 
+      then chmod 664 ~/.ssh/*
+    fi
+    cp $homePersonalSSH/* -t ~/.ssh/
+    chmod 400 ~/.ssh/*
+    chmod 664 ~/.ssh/*.pub
+fi
